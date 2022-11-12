c50cd48147867f528d90c951c0c15a0f3a8c97d6
indarit <109621400+indarit@users.noreply.github.com>
2022-11-12 18:27:54 +0100

    Lesson 1 listo

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <p>CYF Hotel</p>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/d/d2/Hotel_K%C3%A4mp_by_Night_in_Winter_-_panoramio.jpg"
+        alt
+      />
+    </header>
+  );
+};
